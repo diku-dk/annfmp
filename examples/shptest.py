1866418cf71ba5c7df8d3f6d9e70a052172bd97b
Daniel <dmolhave@gmail.com>
2026-04-20 13:14:13 +0200

    New test

---
+++ examples/shptest.py
@@ -0,0 +1,57 @@
+import cv2
+import copy
+import numpy
+
+from annfmp import ANNField
+
+# compute ANN field with openclopt
+print("Computing ANN field with openclopt ...")
+modelOpencl = ANNField(
+    n_neighbors=8,
+    psize=8,
+    dim_reduced=40,
+    height=9,
+    n_subset=1000,
+    algorithm="openclopt",
+    propagate=True,
+    select_best_nn=True,
+    verbose=1,
+    seed=0,
+    n_jobs=16,
+)
+
+# compute ANN field with openmp
+print("\nComputing ANN field with openmp ...")
+modelOpenmp = ANNField(
+    n_neighbors=8,
+    psize=8,
+    dim_reduced=40,
+    leaf_size=128,
+    n_subset=1000,
+    algorithm="openmp",
+    propagate=True,
+    select_best_nn=True,
+    verbose=1,
+    seed=0,
+    n_jobs=16,
+)
+
+flags = []
+
+### Point these to your image pair ------------
+for x in range(5, 7, 2):
+    print(f"\n Current x: {x}\n")
+    image_a = cv2.imread(f"../data/example/1200/{x}.jpg")
+    image_b = cv2.imread(f"../data/example/1200/{x+1}.jpg")
+
+    print("Shape of image A: {}".format(image_a.shape))
+    print("Shape of image B: {}".format(image_b.shape))
+
+    nn_indices_opencl = modelOpencl.fit(image_a, image_b)
+
+    # compute score
+    print("Computing OpenCL overall score ...")
+    patches_a_reconst = modelOpencl.patches_b[nn_indices_opencl]
+    diff = modelOpencl.patches_a.astype(numpy.float32) - patches_a_reconst.astype(numpy.float32)
+    l2Opencl = numpy.mean(numpy.linalg.norm(diff, axis=1))
+    print("Overall L2 score: {}".format(l2Opencl))
